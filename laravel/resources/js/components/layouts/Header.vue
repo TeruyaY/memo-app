3895aa80251d6ef3945d7dd8f1bc02ae80e4cfb8
TeruyaY <terryinwaseda@ruri.waseda.jp>
2026-04-13 15:06:56 +0900

    feature: added header

---
+++ laravel/resources/js/components/layouts/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import DocumentSvg from "../svgs/DocumentSvg.vue"
+</script>
+
+<template>
+    <div class="text-center mb-8">
+        <div class="flex justify-center items-center gap-x-2 mt-5">
+            <DocumentSvg class="w-10 h-10"/>
+            <h3 class="text-4xl font-bold text-primary-500">かんたんメモ</h3>
+        </div>
+        <p class="text-gray-800 mt-2 text-lg">思いついたことをサッと記録しよう</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
