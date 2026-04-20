012fad9e8b6854753ada09f04c980eb91314f99b
Teruya Yamaguchi <terryinwaseda@ruri.waseda.jp>
2026-04-20 12:42:56 +0900

    feature/memo-display-card

---
+++ laravel/resources/js/components/MemoCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+interface Memo {
+    id: number;
+    content: string;
+    created_at: string;
+}
+
+const props = defineProps<{
+    memo: Memo
+}>();
+
+
+</script>
+
+<template>
+    <div class="flex flex-col mx-auto w-full p-4 bg-white rounded-xl shadow hover:shadow-lg transition duration-300 border-[1px] border-primary-100">
+        <h3 class="text-xl mb-2">{{ memo.content }}</h3>
+        <p class="text-lg text-gray-500">{{ memo.created_at }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
