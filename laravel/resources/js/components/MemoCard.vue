051a7cd2a48141b2d03350123601bb5a68a9df83
Teruya Yamaguchi <terryinwaseda@ruri.waseda.jp>
2026-04-20 13:11:35 +0900

    feature/memo-display-count

--- laravel/resources/js/components/MemoCard.vue
+++ laravel/resources/js/components/MemoCard.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+import TrashSvg from "@/components/svgs/TrashSvg.vue";
+import PlusSvg from "@/components/svgs/PlusSvg.vue";
+
+import { ref } from 'vue';
 
 interface Memo {
     id: number;
@@ -10,14 +14,22 @@ const props = defineProps<{
     memo: Memo
 }>();
 
+const isHovered = ref(false);
 
 </script>
 
 <template>
-    <div class="flex flex-col mx-auto w-full p-4 bg-white rounded-xl shadow hover:shadow-lg transition duration-300 border-[1px] border-primary-100">
-        <h3 class="text-xl mb-2">{{ memo.content }}</h3>
-        <p class="text-lg text-gray-500">{{ memo.created_at }}</p>
+    <div @mouseenter="isHovered = true"
+         @mouseleave="isHovered = false"
+        class="flex justify-between items-center mx-auto w-full p-4 bg-white rounded-xl shadow hover:shadow-lg transition duration-300 border-[1px] border-primary-100">
+        <div class="flex flex-col">
+            <h3 class="text-xl mb-2">{{ memo.content }}</h3>
+            <p class="text-lg text-gray-500">{{ memo.created_at }}</p>
+        </div>
+
+        <TrashSvg v-if="isHovered" class="w-8 h-8 text-red-500"/>
     </div>
+
 </template>
 
 <style scoped>
